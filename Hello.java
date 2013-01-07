8e1d6eb8a0a861659fce01d32f30bc58a5a363e4
Vlad Firoiu <vlad@vlad-laptop-ubuntu.(none)>
2013-01-07 01:34:44 -0500

    NANO IS A TEXT EDITOR

--- Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello{
-	public static void main(String[] args){
-		System.out.println("Hi Dame and Vlad and Tim!");
-        System.out.println("Hi Josh!");
-        System.out.println("Thanks Damien!");
-        // I can't believe I'm not butter
-        // oof
-	}
-}
