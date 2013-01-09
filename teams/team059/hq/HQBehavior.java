9eb233b8e4ec8f2a088d6de3fb39688aa57331b0
Josh Alman <jalman@mit.edu>
2013-01-09 18:53:32 -0500

    soldiers build things! In particular, GENERATORs.

--- teams/team059/hq/HQBehavior.java
+++ teams/team059/hq/HQBehavior.java
@@ -16,13 +16,12 @@ public class HQBehavior extends RobotBehavior {
 		super(therc);
 		MapLocation[] mines = therc.senseNonAlliedMineLocations(therc.getLocation(), 100000);
 		mineDensity = (mines.length / (double) therc.getMapHeight()) / (double) therc.getMapWidth();
-		upgradeList = mineDensity > 0.7 ? DENSE_UPGRADES : SPARSE_UPGRADES;
+		upgradeList = mineDensity > 0.4 ? DENSE_UPGRADES : SPARSE_UPGRADES;
 	}
 
 	@Override
 	public void run() {
 		if(rc.getTeamPower() - 40.0 > 15.0 || Clock.getRoundNum() < 40) {
-			System.out.println("asdf");
 			try {
 				rc.setIndicatorString(0, Double.toString(rc.getTeamPower()));
 				buildSoldier(rc.getLocation().directionTo(rc.senseEnemyHQLocation()));
