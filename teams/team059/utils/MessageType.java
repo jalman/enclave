6b7cb7e9d498790e777716be60d979f0d7061898
Josh Alman <jalman@mit.edu>
2013-01-08 22:30:46 -0500

    Some basic utils and MessageType

---
+++ teams/team059/utils/MessageType.java
@@ -0,0 +1,7 @@
+package team059.utils;
+
+public enum MessageType {
+	ENCAMPMENT_TYPE,
+	ATTACK_LOCATION,
+	DEFEND_LOCATION
+}
---
+++ teams/team059/utils/Utils.java
@@ -0,0 +1,47 @@
+package team059.utils;
+
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.Team;
+
+public class Utils {
+	RobotController rc = null;
+	Team myTeam = null;
+	
+	public void initUtils(RobotController rc) {
+		this.rc = rc;
+	}
+	
+	public boolean isEnemyMine(Team team) {
+		return !(team == myTeam() || team == null);
+	}
+
+	public boolean isEnemyMine(MapLocation loc) {
+		return isEnemyMine(rc.senseMine(loc));
+	}
+	
+	public Team myTeam() {
+		if(myTeam != null) {
+			return myTeam;
+		}
+		myTeam = rc.getTeam();
+		return myTeam;
+	}
+	
+	public void broadcast(MessageType type, int data) {
+		try {
+			rc.broadcast(type.hashCode() % 10000, data);			//change from hashCode to something reasonable
+		} catch (GameActionException e) {
+		}	
+	}
+	
+	public int readBroadcast(MessageType type) {
+		try {
+			return rc.readBroadcast(type.hashCode() % 10000);		//change from hashCode to something reasonable
+		} catch (GameActionException e) {
+			return -1;
+		}
+	}
+	
+}
