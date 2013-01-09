383245f11e61b41fded8e4bbbe2cfc6059700eb2
Timothy <timchu@mit.edu>
2013-01-09 17:30:00 -0500

    micro blah

--- teams/team059/soldiers/micro/micro.java
+++ teams/team059/soldiers/micro/micro.java
@@ -1,95 +1,43 @@
-package team059.soldiers;
 
-import team059.RobotBehavior;
-import battlecode.common.*;
 
-public class SoldierBehavior extends RobotBehavior {
+Team enemy;
+boolean retreatMode;
 
-	MapLocation encampTarget = null;
-	
-	
-	public SoldierBehavior(RobotController therc) {
-		super(therc);
+boolean enemyNearby() // tests whether an enemy is closeby.
+{
+	if (senseNearbyGameObjecs(RobotType.SOLDIER, 14, enemy).length == 0) // condition
+	{
+		return false;
 	}
+	return true;
+}
 
-	@Override
-	public void run() {
-		try {
-			if (rc.isActive()) {
-				
-				//see if there is an encampment nearby to take
-				if(encampTarget == null) {
-					MapLocation[] encampments = rc.senseEncampmentSquares(rc.getLocation(), 16, Team.NEUTRAL);
-					if(encampments.length > 0) {
-						int maxdist = 20;
-						for(MapLocation encampment : encampments) {
-							int dist = encampment.distanceSquaredTo(rc.getLocation());
-							if(dist < maxdist) {
-								maxdist = dist;
-								encampTarget = encampment;
-							}
-						}
-					}
-				} else { //see if the encampment is taken
-					GameObject there = rc.senseObjectAtLocation(encampTarget);
-					if(there != null) {
-						encampTarget = null;
-					}
-				}
-				
-				rc.setIndicatorString(0, encampTarget + "");
-				
-				
-				if(encampTarget != null && encampTarget.distanceSquaredTo(rc.getLocation()) == 0) {
-					try{
-						rc.captureEncampment(RobotType.GENERATOR);
-					} catch(Exception e) {
-						e.printStackTrace();
-					}
-				} else {
-					
-					Direction dir = encampTarget == null ? rc.getLocation().directionTo(rc.senseEnemyHQLocation()) : rc.getLocation().directionTo(encampTarget);
-					
-					if(!moveMine(dir)) {
-						if(!moveMine(dir.rotateLeft()))
-							moveMine(dir.rotateRight());
-					}
-					
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+boolean hasEnoughAllies() // tests whether there are enough allies nearby to engage.
+{
+	if(true)// wants to have allies who can sense enemies. Use the messaging system to do this.
+	{
+		return true;
 	}
-	
-	public boolean moveMine(Direction dir) {
-		Team mine = rc.senseMine(rc.getLocation().add(dir));
-		if(mine != null && mine != rc.getTeam()) {
-			try {
-				rc.defuseMine(rc.getLocation().add(dir));
-				return true;
-			} catch (GameActionException e) {
-				e.printStackTrace();
-			}
-		}
+	return false;
+}
+
+// two modes; normal mode
+void battleState() // determines whether to retreat or not. Run battlestate every turn.
+{
+	if(enemyNearby() && !hasEnoughAllies())
+	{
+		retreatMode = true;
+	}
+	else
+	{
+		retreatMode = false;
+	}
+}
+
+void retreat()
+{
+	if (retreatMode == true)
+	{
 		
-		if(rc.canMove(dir)) {
-			try {
-				rc.move(dir);
-			} catch (GameActionException e) {
-				e.printStackTrace();
-			}
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public void buildEncampment(Direction dir) {
-
-	}
-
-	public void researchUpgrade(Upgrade upg) {
-
 	}
 }
