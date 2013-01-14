5406f825a562a5367043d6a42e2eb3da8820d459
jiangty <jiangty@mit.edu>
2013-01-14 14:11:13 -0500

    oops here's BugMove.java

---
+++ teams/team059/movement/BugMove.java
@@ -0,0 +1,88 @@
+package team059.movement;
+
+import battlecode.common.*;
+import team059.RobotBehavior;
+import static team059.utils.Utils.*;
+
+public class BugMove extends NavAlg {
+	
+	public Direction lastDir;
+	private Direction wantDir, tryDir;
+	private boolean bugging, hugLeft, waiting;
+	private MapLocation beganBugging;
+	
+	public BugMove(RobotBehavior rb, MapLocation finish) {
+		super(rb, finish);
+		//lastDir = Direction.NONE;
+	}
+	
+	public void recompute() {
+	}
+	
+	public void recompute(MapLocation finish) {
+		this.finish = finish;
+	}
+	
+	/*public void recompute(MapLocation finish, Direction lastDir) {
+		this.lastDir = lastDir;
+		this.finish = finish;
+	}
+	*/
+	
+	public Direction getNextDir() {
+		Direction d = getNextDirPrivate();
+		RC.setIndicatorString(2, "Bugging: " + bugging + ", hugLeft: " + hugLeft + ", direction: " + d);
+		lastDir = d;
+		return d;
+	}
+		
+	private Direction getNextDirPrivate() {
+		this.start = RC.getLocation();
+		
+		wantDir = start.directionTo(finish);
+		
+		if(!bugging) {
+			if(canMoveNoMine(wantDir)) {
+				return wantDir;
+			} else {
+				bugging = true;
+				beganBugging = RC.getLocation();
+				hugLeft = ( naiveDistance(start.add(wantDir.rotateRight()), finish) 
+								< naiveDistance(start.add(wantDir.rotateLeft()), finish) ? true : false );
+			}
+		}
+		
+		// if bugging...
+
+		if(canMoveNoMine(wantDir) && start.distanceSquaredTo(finish) < beganBugging.distanceSquaredTo(finish)) {
+			bugging = false;
+			return wantDir;
+		} 
+		
+		if(hugLeft) {
+			tryDir = lastDir.rotateRight().rotateRight();
+			for(int i=0; i<8; i++) {
+				tryDir = tryDir.rotateLeft();
+				if(canMoveNoMine(tryDir) && !lastDir.equals(tryDir.opposite())) {
+					return tryDir;
+				}
+			}
+		} else {
+			tryDir = lastDir.rotateLeft().rotateLeft();
+			for(int i=0; i<8; i++) {
+				tryDir = tryDir.rotateRight();
+				if(canMoveNoMine(tryDir) && !lastDir.equals(tryDir.opposite())) {
+					return tryDir;
+				}
+			}
+		}
+		return Direction.NONE;
+
+	}
+	
+	private boolean canMoveNoMine(Direction d) {
+		//System.out.println(d);
+		if(d==null) return true;
+		return ( RC.canMove(d) && !isEnemyMine(start.add(d)) );
+	}
+}
