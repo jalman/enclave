a864b294d5ac9f7826b9fa457ef2554ca352aa92
Josh Alman <jalman@mit.edu>
2013-01-14 23:59:01 -0500

    artillery are pro

--- teams/team059/encampment/ArtilleryBehavior.java
+++ teams/team059/encampment/ArtilleryBehavior.java
@@ -1,5 +1,6 @@
 package team059.encampment;
 
+import battlecode.common.Clock;
 import battlecode.common.GameActionException;
 import battlecode.common.MapLocation;
 import battlecode.common.Robot;
@@ -31,8 +32,8 @@ public class ArtilleryBehavior extends RobotBehavior {
 			{false, false, false, false, false, true, true, true, true, true, true, true, false, false, false, false, false},
 			{false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false}
 	};
-	
-	
+
+
 	public ArtilleryBehavior(RobotController rc) {
 		super(rc);
 	}
@@ -40,93 +41,129 @@ public class ArtilleryBehavior extends RobotBehavior {
 	@Override
 	public void run() {
 
-		if(!rc.isActive())
-			return;
-		
-		MapLocation me = rc.getLocation();
-		
-		Robot[] robots = rc.senseNearbyGameObjects(Robot.class, 98);
-		int[][] weight = new int[17][17];
-		
-		RobotInfo info;
-		
-		for(Robot robot : robots) {
-			try {
-				
-				info = rc.senseRobotInfo(robot);
-				
-				int x = info.location.x;
-				int y = info.location.y;
-				
-				x -= me.x;
-				y -= me.y;
-				
-				weight[x + 8][y + 8] = weightedWeight(info.type,info.team);
-				
-			} catch (GameActionException e) {
-				e.printStackTrace();
+		if(rc.isActive()){
+
+
+			MapLocation me = rc.getLocation();
+
+			Robot[] robots = rc.senseNearbyGameObjects(Robot.class, 98);
+			int[][] weight = new int[17][17];
+			
+			int[] enemiesX = new int[robots.length];
+			int[] enemiesY = new int[robots.length];
+			int numEnemies = 0;
+
+			RobotInfo info;
+
+			for(Robot robot : robots) {
+				try {
+
+					info = rc.senseRobotInfo(robot);
+
+					int x = info.location.x;
+					int y = info.location.y;
+
+					x -= me.x;
+					y -= me.y;
+					
+					x += 8;
+					y += 8;
+
+					if(0 <= x && x <= 16 && 0 <= y && y <= 16) {
+						if (info.team == Utils.ALLY_TEAM) {
+							weight[x][y] = -weight(info.type);
+
+						} else {
+							weight[x][y] = weight(info.type);
+							enemiesX[numEnemies] = x;
+							enemiesY[numEnemies] = y;
+							numEnemies++;
+						}
+					}
+
+				} catch (GameActionException e) {
+					e.printStackTrace();
+				}
+
 			}
 			
-
 			int attackX = 0;
 			int attackY = 0;
-			int attackWeight = 0;
-			
-			for(int i = 0; i < 17; i++) {
-				for(int j = 0; j < 17; j++) {
-					if(!IN_RANGE[i][j])
-						continue;
-					
-					int val = 0;
-					val += weight[i-1][j-1];
-					val += weight[i-1][j+1];
-					val += weight[i+1][j-1];
-					val += weight[i+1][j+1];
-					val += weight[i][j-1];
-					val += weight[i][j+1];
-					val += weight[i-1][j];
-					val += weight[i+1][j];
-					val += weight[i][j]*2;
-					
-					if(val > attackWeight) {
-						attackWeight = val;
-						attackX = i;
-						attackY = j;
+			int attackWeight = -1000;
+
+			for(int n = 0; n < numEnemies; n++) {
+				for(int i = enemiesX[n] - 1; i <= enemiesX[n] + 1; i++){
+					for(int j = enemiesY[n] - 1; j <= enemiesY[n] + 1; j++){
+						if(i <= 0 || i >= 16 || j <= 0 || j >= 16) {
+							continue;
+						}
+						
+
+						if(!IN_RANGE[i][j])
+							continue;
+						
+
+						int val = 0;
+						val += weight[i-1][j-1];
+						val += weight[i-1][j+1];
+						val += weight[i+1][j-1];
+						val += weight[i+1][j+1];
+						val += weight[i][j-1];
+						val += weight[i][j+1];
+						val += weight[i-1][j];
+						val += weight[i+1][j];
+						val += weight[i][j]*2;
+						
+
+						if(val > attackWeight) {
+							attackWeight = val;
+							attackX = i;
+							attackY = j;
+						}
 					}
 				}
 			}
-			
-			
+
+
 			if(attackX != 0 || attackY != 0) {
 				try {
 					rc.attackSquare(new MapLocation(me.x + attackX - 8, me.y + attackY - 8));
+					rc.setIndicatorString(0, weight[attackX - 1][attackY - 1] + " " + weight[attackX][attackY - 1] + " " + weight[attackX + 1][attackY - 1]);
+					rc.setIndicatorString(1, weight[attackX - 1][attackY] + " " + weight[attackX][attackY] + " " + weight[attackX + 1][attackY]);
+					rc.setIndicatorString(2, weight[attackX - 1][attackY + 1] + " " + weight[attackX][attackY + 1] + " " + weight[attackX + 1][attackY + 1]);
 				} catch (GameActionException e) {
 					e.printStackTrace();
 				}
 			}
+			
 		}
 	}
-	
-	private int weightedWeight(RobotType type, Team team) {
-		return (team == Utils.ALLY_TEAM ? -1 : 1) * weight(type);
-	}
-	
+
 	private int weight(RobotType type) {
 		switch(type) {
-			case SOLDIER:
-				return 2;
-			case HQ:
-				return 6;
-			case SHIELDS:
-				return 4;
-			case MEDBAY:
-				return 4;
-			case ARTILLERY:
-				return 4;
-			default:
-				return 1;
+		case SOLDIER:
+			return 2;
+		case HQ:
+			return 6;
+		case SHIELDS:
+			return 4;
+		case MEDBAY:
+			return 4;
+		case ARTILLERY:
+			return 4;
+		default:
+			return 1;
 		}
 	}
 	
+	@Override
+	public void beginRound() {
+	}
+	
+	@Override
+	public void endRound() {
+	}
+	
+
 
 }
