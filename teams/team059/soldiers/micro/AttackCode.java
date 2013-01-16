a5d72e2611702b4ea356bb036eb38191b19f06a8
Vlad Firoiu <vlad@vlad-desk.(none)>
2013-01-15 22:12:59 -0500

    Fixed a small compilation error.

--- teams/team059/soldiers/micro/AttackCode.java
+++ teams/team059/soldiers/micro/AttackCode.java
@@ -2,7 +2,7 @@ package team059.soldiers.micro;
 
 import team059.movement.Mover;
 import team059.soldiers.SoldierBehavior;
-import team059.utils.Utils;
+import static team059.utils.Utils.*;
 import battlecode.common.*;
 
 public class AttackCode {
@@ -10,7 +10,6 @@ public class AttackCode {
 	 * Nearby allies will flock to a battle.
 	 */
 	Micro micro;
-	RobotController rc;
 	public final Mover mover;
 	MapLocation target;
 	SoldierBehavior sb;
@@ -18,7 +17,6 @@ public class AttackCode {
 	public AttackCode(Micro micro){
 		this.micro = micro;
 		mover = micro.mover;
-		rc = micro.rc;
 	}
 	
 	public void run(){
