b3febacc6780cb5ffb663b22e3c97dc55654b09b
Vlad Firoiu <vladfi1@mit.edu>
2013-01-28 20:48:41 -0500

    Fixed coordination of expanding.

--- teams/team059/soldiers/ExpandTask.java
+++ teams/team059/soldiers/ExpandTask.java
@@ -1,8 +1,6 @@
 package team059.soldiers;
 
-import team059.Strategy;
-import team059.movement.Mover;
-import team059.movement.NavType;
+import team059.Encampments;
 import battlecode.common.Clock;
 import battlecode.common.Direction;
 import battlecode.common.GameActionException;
@@ -11,19 +9,18 @@ import battlecode.common.MapLocation;
 import battlecode.common.RobotType;
 import static team059.utils.Utils.*;
 
-public class ExpandTask extends TravelTask {
+public class ExpandTask extends AttackTask {
 	
 	private final RobotType buildType;
 	private final MapLocation badA, badB;
 	private boolean init = true;
-	int count = 0;
 	
 	public ExpandTask(MapLocation encampment) {
 		this(encampment, parameters.greed, null);
 	}
 
 	public ExpandTask(MapLocation encampment, int priority, RobotType buildType) {
-		super(encampment, priority, 0);
+		super(encampment, priority);
 		this.buildType = buildType;
 		
 		Direction dirToEnemy = ALLY_HQ.directionTo(ENEMY_HQ);
@@ -33,6 +30,7 @@ public class ExpandTask extends TravelTask {
 	
 	@Override
 	public boolean done() {
+		
 		try {
 			if(!currentLocation.equals(destination) && RC.canSenseSquare(destination)) {
 				GameObject object = RC.senseObjectAtLocation(destination);
@@ -44,20 +42,20 @@ public class ExpandTask extends TravelTask {
 			e.printStackTrace();
 		}
 
+		if(Encampments.value[destination.x][destination.y] > Clock.getRoundNum() + appeal()) {
+			return true;
+		}
+		
 		return false;
 	}
 
 	@Override
 	public int appeal() {
-		if(strategy.equals(Strategy.NUCLEAR) || count > 50) {
-			return -1;
-		}
 		return destination.equals(badA) || destination.equals(badB) ? -10000 : (int) (super.appeal() - RC.senseCaptureCost() / 5);
 	}
 
 	@Override
 	public void execute() throws GameActionException {
-		count++;
 		if(init) {
 			messagingSystem.writeTakingEncampmentMessage(destination, appeal());
 			init = false;
